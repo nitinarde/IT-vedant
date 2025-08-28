b7f4ea9d425812bcdb8d91379fd544b56bacabaf
nitinarde <125584497+nitinarde@users.noreply.github.com>
2025-08-28 19:40:10 +0530

    Add files via upload

---
+++ Buttons.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function Buttons() {
+  let counter = 15;
+
+  const addvalue = () => {
+    counter = 1 + counter;
+    console.log(counter);
+  };
+
+  const removevalue = () => {
+    counter = counter - 1;
+    console.log(counter);
+  };
+
+  return (
+    <div>
+      <h1>React vite app</h1>
+      <button onClick={addvalue}>Add Value</button>
+      <button onClick={removevalue}>Remove value</button>
+    </div>
+  );
+}
